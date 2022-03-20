160e0dc0d09b473fe16b94ac18c58944711b9e98
vvladislavovichh <38782177+vladislavovhich@users.noreply.github.com>
2022-03-20 17:09:51 +0300

    lab6 new

--- lab6/lab6.js
+++ lab6/lab6.js
@@ -1,94 +1,218 @@
 "use strict";
 
-let process = [{priority: 3, duration: 3, isCompleted:false},
-               {priority: 4, duration: 3, isCompleted:false}, 
-               {priority: 1, duration: 9, isCompleted:false}, 
-               {priority: 2, duration: 2, isCompleted:false}, 
-              ];
+let div = document.getElementById("tableDiv");
+let btn = document.getElementById("btn");
 
-let states = [];
-let statesN = [];
+let process = [];
+let processN = +prompt("Enter amount of processess: ");
+
+for (let i = 0; i < processN; i++)
+{
+    let priority = +prompt("Enter priority of process: ");
+    let duration = +prompt("Enter duration of process: ");
+    let appearance = +prompt("Enter appearance of process: ");
+
+    process.push({priority, duration, remained: duration, appearance, isCompleted: false, states: []});
+}
+
 let table = [];
+let statesN = [];
+let wAmount = 0, eAmount = 0;
 
-for (let i = 0; i < process.length; i++) 
-    states.push([]);
-
-for (let i = 0; i < process.length; i++)
-    for (let j = 0; j < process.length - 1; j++)
-        if (process[j + 1].duration < process[j].duration)
-        {
-            let pch = process[j + 1];
-            process[j + 1] = process[j];
-            process[j] = pch;
-        }
-
+let isPrEx = false, f = false;
 let numOfExPr = 0;
 let q = 0;
 
-while (numOfExPr != 4 )
+btn.addEventListener("click", (e) => 
 {
+    if (process.some(el => !el.isCompleted))
+    {
+        f = false;
 
-    if (process[numOfExPr].duration != 0)
-    {
-        console.log(`Process ${numOfExPr} is executing with priority ${process[numOfExPr].priority}`);
-        
-        for (let i = 0; i < process.length; i++)
-            if (i != numOfExPr && !process[i].isCompleted)
-                states[i].push(["W", process[i].priority]);
-            else if (process[i].isCompleted)
-                states[i].push(["C", process[i].priority]);
-            else if (numOfExPr == i)
-                states[i].push(["E", process[i].priority]);
-        
-        process[numOfExPr].duration--;
-    } 
-    else if (process[numOfExPr].duration == 0) 
-    {
-        console.log(`Process ${numOfExPr} is over!`);
-        
-        process[numOfExPr].isCompleted = true;
-        numOfExPr++;
-    }
+        if (!isPrEx) 
+        {
+            let processQ = findQprocess(process);
+
+            if (processQ.length)
+            {
+                numOfExPr = findMaxProcess(processQ);
+                isPrEx = true;
+
+                process = putStates(process, processQ, numOfExPr);
+                
+                process[numOfExPr].remained--;
+            } 
+            else
+                process = putStates(process, processQ, numOfExPr)
+        } 
+        else if (isPrEx && process[numOfExPr].remained)
+        {
+            let processQ = findQprocess(process);
+
+            process = putStates(process, processQ, numOfExPr);
+
+            process[numOfExPr].remained--;
+        }
+        else if (isPrEx && !process[numOfExPr].remained)
+        {
+            process[numOfExPr].isCompleted = true;
+
+            console.log(`Process with priority ${process[numOfExPr].priority} is over!\n`);
+
+            isPrEx = false;
+
+            f = true;
+        }
+
+        div.innerHTML = "";
+        showQuantTable(div, process, q);
+
+        if (!f) {
+            if (isPrEx)
+                console.log(`Process with priority ${process[numOfExPr].priority} is executing!`);
+
+            statesN.push(q++);
+        }
+    } else
+        showTable();
+});
+
+btn.click();
+
+function findMaxProcess(process) {
+    return process.sort((el1, el2) => 
+            {
+                if (el1.pr.priority == el2.pr.priority)
+                    if (el1.pr.duration < el2.pr.duration) 
+                        return -1;
+                    else 
+                        return 1;
+                else if (el1.pr.priority < el2.pr.priority)
+                    return -1;
+                else 
+                    return 1;
+            })[0].i;
+}
+function findQprocess(process) {
+    let arr = [];
+
+    process.forEach((pr, i) => pr.appearance <= q && !pr.isCompleted && arr.push({pr, i}));
     
-    statesN.push(q++);
+    return arr;
+}
+function putStates(process, processQ, numOfExPr)
+{
+    process.forEach((el, i) => 
+    {
+        if (i != numOfExPr && !processQ.some(e => e.i == i)) 
+            el.states.push(" ");
+        else if (i != numOfExPr && processQ.some(e => e.i != i))
+            el.states.push("W");
+        else if (i == numOfExPr && !el.isCompleted)
+            el.states.push("E");
+    });
+
+    return process;
 }
 
-table.push(["#", ...statesN.slice(0, Math.max(...states.map(el => el.length)))]);
-
-for (let i = 0; i < process.length; i++)
-    table.push(...states[i]);
-
-if (window) 
+function showQuantTable(el, process, q)
 {
-    document.write("<table border='1'>");
+    let str1 = ["#", 'Priority', 'Duration', 'Remained', 'Appearance'];
 
-    document.write("<thead>");
-    document.write("<tr>");
+    let b = document.createElement("b");
+    b.innerHTML = `Current quant is ${q}`;
 
-    table[0].forEach((el) => {
-        document.write(`<td>${el}</td>`);
+    el.append(b);
+    el.append(document.createElement("br"));
+
+    let table = document.createElement("table");
+    let thead = document.createElement("thead");
+    let tbody = document.createElement("tbody");
+
+    table.style.borderCollapse = "collapse";
+    table.setAttribute("border", "1");
+
+    str1.forEach((el) => {
+        let td = document.createElement("td");
+
+        td.innerHTML = el;
+
+        thead.append(td);
     })
 
-    document.write("</tr>");
-    document.write("</thead>");
+    process.forEach((el, ind) => {
+        let tr = document.createElement("tr");
+        let td = document.createElement("td");
 
-    document.write("<tbody>");
+        td.innerHTML = ind;
+
+        tr.append(td);
+
+        Object.values(el).slice(0, 4).forEach((propName) =>
+        {
+            let td = document.createElement("td");
+
+            td.innerHTML = propName;
+
+            tr.appendChild(td);
+        });
+
+        tbody.append(tr);
+    })
+
+    table.append(tbody);
+    table.append(thead);
+
+    el.append(table);
+}
+
+function showTable() 
+{
+    table.push(["#", ...statesN]);
+
+    let t = document.createElement("table");
+    let thead = document.createElement("thead");
+    let tbody = document.createElement("tbody");
+
+    t.style.borderCollapse = "collapse";
+    t.setAttribute("border", "1");
+
+    let tr = document.createElement("tr");
+
+    table[0].forEach((el) => {
+        let td = document.createElement("td");
+
+        td.innerHTML = el;
+
+        tr.append(td);
+    })
+
+    thead.append(tr);
+    t.append(thead);
 
     for (let i = 0; i < process.length; i++)
     {
-        document.write("<tr>");
-        document.write(`<td>${states[i][0][1]}</td>`);
+        let tr = document.createElement("tr");
 
-        states[i].forEach((el) => {
-            (el[0] == "C") ? document.write(`<td></td>`) : document.write(`<td>${el[0]}</td>`);  
+        let td = document.createElement("td");
+
+        td.innerHTML = process[i].priority;
+
+        tr.append(td);
+
+        
+        process[i].states.forEach((state) => {
+            td = document.createElement("td");
+
+            td.innerHTML = state;
+
+            tr.append(td);
         });
 
-        document.write("</tr>");
+        tbody.append(tr);
     }
 
-    document.write("</tbody>");
+    t.append(tbody);
 
-    document.write("</table>");
+    document.body.appendChild(t);
 }
-
-
